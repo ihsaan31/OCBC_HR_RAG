e8ea897a533d1b7f5fccda88b6ab8ab5dc0705d7
ihsan <ihsanzombieihsan@gmail.com>
2024-08-07 22:43:23 +0700

    fix

--- rag_chain.py
+++ rag_chain.py
@@ -1,6 +1,4 @@
 from langchain import hub
-from langchain_chroma import Chroma
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
