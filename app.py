cbbd74f72034418c148f3db23a5e4e8e98950487
ihsan <ihsanzombieihsan@gmail.com>
2024-08-07 22:30:10 +0700

    added streamlit

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import time
+import random
+import string
+
+from rag_chain import caller
+
+# sess_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+sess_id = "W56PNA34XM"
+
+# Streamlit page configuration
+st.set_page_config(page_title="Chat Application", layout="wide")
+
+# Initialize chat history in session state
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "How may I help you today?"}
+    ]
+
+if "current_response" not in st.session_state:
+    st.session_state.current_response = ""
+
+# We loop through each message in the session state and render it as
+# a chat message.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# We take questions/instructions from the chat input to pass to the LLM
+if user_prompt := st.chat_input("Your message here", key="user_input"):
+
+    # Add our input to the session state
+    st.session_state.messages.append(
+        {"role": "user", "content": user_prompt}
+    )
+
+    # Add our input to the chat window
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    with st.spinner("Thinking ..."):
+        response = caller(user_prompt)
+    # Add the response to the session state
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response}
+    )
+
+    # Add the response to the chat window
+    with st.chat_message("assistant"):
+        st.markdown(response)
